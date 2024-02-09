7e419ea50950cccce01c2feb02815667ca2d0e29
David Hernández <rojohernandez75@gmail.com>
2024-02-09 17:07:43 -0600

    Initial Commit

---
+++ Nice Places/Nice_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nice_PlacesApp.swift
+//  Nice Places
+//
+//  Created by Alan David Hernández Trujillo on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Nice_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
